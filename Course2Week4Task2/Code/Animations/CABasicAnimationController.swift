68f335dcf7161dda6b33033936f3d43f3ee2593f
Ekaterina Volkova <ekaterina.volkova@zalando.fi>
2022-09-20 16:44:05 +0300

    Initial Commit

---
+++ Course2Week4Task2/Code/Animations/CABasicAnimationController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Course2Week4Task2
+//
+//  Copyright © 2018 e-Legion. All rights reserved.
+//
+
+import UIKit
+
+class CABasicAnimationController: UIViewController {
+    
+    @IBOutlet weak var orangeView: UIView!
+    @IBOutlet weak var cyanView: UIView!
+    @IBOutlet weak var blueView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    
+}
