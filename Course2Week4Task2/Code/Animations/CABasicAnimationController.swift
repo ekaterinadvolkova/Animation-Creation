eb1eebfbf95f9d3cb52a86e116bee0c8f156c5b1
Ekaterina Volkova <ekaterina.volkova@zalando.fi>
2022-09-23 15:54:22 +0300

    Add CSBasicAnimattion

--- Course2Week4Task2/Code/Animations/CABasicAnimationController.swift
+++ Course2Week4Task2/Code/Animations/CABasicAnimationController.swift
@@ -14,4 +14,24 @@ class CABasicAnimationController: UIViewController {
     @IBOutlet weak var blueView: UIView!
     @IBOutlet weak var greenView: UIView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let orangeTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(performCABasicAnimation))
+        self.orangeView.addGestureRecognizer(orangeTapRecognizer)
+    }
+    
+    @IBAction func performCABasicAnimation(){
+        
+        let animatableView = self.orangeView!
+        let animation = CABasicAnimation(keyPath: #keyPath(CALayer.cornerRadius))
+        animation.duration = 1.0
+        animation.fromValue = orangeView.layer.cornerRadius
+        animation.toValue = orangeView.frame.height / 2
+        animation.timingFunction = CAMediaTimingFunction(name: .easeOut)
+        
+        animatableView.layer.add(animation, forKey: "cornerRadius")
+        orangeView.layer.cornerRadius = orangeView.frame.height / 2
+        
+    }
+    
 }
