54f6d33a18ff6c90c51a0a0106657b423a25688a
Kateridzhe <62830083+ekaterinadvolkova@users.noreply.github.com>
2022-10-11 14:32:16 +0300

    Update CATransitionController.swift

--- Course2Week4Task2/Code/Animations/CATransitionController.swift
+++ Course2Week4Task2/Code/Animations/CATransitionController.swift
@@ -12,6 +12,25 @@ class CATransitionController: UIViewController {
     @IBOutlet weak var textLabel: UILabel!
 
     @IBAction func swipeHandler(_ sender: UISwipeGestureRecognizer) {
+        CATransaction.begin()
+        CATransaction.setCompletionBlock {
+            let animation = CATransition()
+            animation.duration = 1.0
+            animation.type = .fade
+            animation.timingFunction = CAMediaTimingFunction(name: .easeOut)
+            self.textLabel.layer.add(animation, forKey: "animation")
+            self.textLabel.text = "Initial Text"
+            self.textLabel.textColor = .orange
+        }
+        
+        let swipeRight = CATransition()
+        swipeRight.type = .moveIn
+        swipeRight.duration = 1.0
+        swipeRight.timingFunction = CAMediaTimingFunction(name: .easeOut)
+        textLabel.layer.add(swipeRight, forKey: "swipe")
+        textLabel.text = "Sliding!"
+        textLabel.textColor = .green
+        CATransaction.commit()
     }
 
 }
