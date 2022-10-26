6aef54170f9d57d135c292d8fb7b0730139adbea
Kateridzhe <62830083+ekaterinadvolkova@users.noreply.github.com>
2022-10-26 21:06:24 +0300

    Update UIViewAnimationController.swift

--- Course2Week4Task2/Code/Animations/UIViewAnimationController.swift
+++ Course2Week4Task2/Code/Animations/UIViewAnimationController.swift
@@ -13,8 +13,18 @@ class UIViewAnimationController: UIViewController {
     @IBOutlet weak var flipButton: UIButton!
     
     @IBAction func animationViewTapHandler(sender: UITapGestureRecognizer) {
+         UIView.animate(withDuration: 1.0, delay: 0.5, options: [.curveEaseInOut], animations:{
+            var endPoint = self.animationView.center
+            endPoint.x = self.view.bounds.size.width - self.animationView.bounds.size.width / 2
+            self.animationView.center = endPoint
+            self.animationView.transform = CGAffineTransform(rotationAngle: .pi)
+        })
     }
     
     @IBAction func flipButtonTapHandler(sender: UIButton) {
+        UIView.animate(withDuration: 1.0, delay: 0, options: [.curveEaseInOut], animations: {
+            self.view.transform = self.view.transform.rotated(by: .pi)
+            self.flipButton.transform = self.flipButton.transform.rotated(by: .pi)
+        })
     }
 }
